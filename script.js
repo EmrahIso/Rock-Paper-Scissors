660f1308d4b8de2afb5892d5d66baa521f965181
Emrah Isovic <emrahisovic20@gmail.com>
2023-12-30 19:08:22 +0100

    Updated pseudocode

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // 1. Write a function getComputerChoice() that gets computer choice 
 // 2. Prompt for user selection
 // 3. Make choices case-insensitive 
-// 4. Compare the values ​​of these two functions inside another function
+// 4. Compare the values ​​of these two functions inside another function playRound
 // 5. If the choices are the same, restart the round and write a tie in the console
 // 6. Else return a string that declares the winner of the round
 // 7. Write a function game() and use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end
